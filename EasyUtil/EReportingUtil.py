a2f6df3c9214a82f519131178cfb561d9450ea95
Matt <matthewzou1997@outlook.com>
2019-08-21 23:48:55 +0800

    improved plot

--- EasyUtil/EReportingUtil.py
+++ EasyUtil/EReportingUtil.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 def plot_performance(performance_df):
     plt.figure(figsize=(12, 6))
     plt.subplot(211)
+    plt.ylim([-1, 0])
     plt.fill_between(performance_df.index, performance_df['drawdown'], 0, label='drawdown', color='gray', alpha=0.5)
     plt.twinx()
     plt.plot(performance_df['total_return'], label='total_return')
@@ -17,8 +18,8 @@ def plot_performance(performance_df):
     plt.axhline(0, c='k')
     plt.legend()
     plt.subplot(212)
+    plt.ylim([0, 1])
     plt.fill_between(performance_df.index, performance_df['position_pct0'], 0, label='position_pct0', alpha=0.5)
-    plt.axhline(0)
     plt.legend()
     plt.tight_layout()
     plt.show()
